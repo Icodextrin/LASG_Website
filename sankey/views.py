4fc2fa63fc9af2fb9ecd7cec209b2703c24391e2
Jack Clarke <jackclar@.iu.edu>
2021-04-21 13:00:23 -0400

    sankey now outputs all sankey graphs. still empty csv however.

--- sankey/views.py
+++ sankey/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.conf import settings
 from scripts import sankey
 from scripts import panda_converter
+import os
 
 # Create your views here.
 
 
 def sankey_view(request):
+    path = settings.MEDIA_ROOT
     panda_converter.count_interactions()
     sankey.sankey_graph()
-    return render(request, 'sankey.html')
+    files = os.listdir(path + '/graphs/sankey/')
+    return render(request, 'sankey.html', {'files': files})
