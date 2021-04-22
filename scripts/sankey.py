b420096d5aab155df3dd645c4f7c1186b32dfcaa
Jack Clarke <jackclar@.iu.edu>
2021-04-22 12:56:25 -0400

    Title names to sankey graph.

--- scripts/sankey.py
+++ scripts/sankey.py
@@ -12,11 +12,17 @@ def sankey_graph():
     # list all of the json files of that type
     file_arr = glob.glob(path + "/csv/proj_interactions/" + "*_interactions.csv")
     # loop through all of the files
+    lookup = pd.read_csv(path + "/csv/projects.csv")
     for f in file_arr:
         df = pd.read_csv(f)
+        names = f.split("_")
+        names = names[2].split("\\")
+
+        col = lookup.loc[lookup['id']==int(names[1])]
+        #print(col.iloc[0][4])
         label_1 = df["commentor.name"]
         label_2 = df["mention.name"]
-        label = label_1.append(label_2,ignore_index=True).unique()
+        label = label_1.append(label_2, ignore_index=True).unique()
         source_1 = df["commentor.name"].tolist()
         target_1 = df["mention.name"].tolist()
         value_1 = df["count"].tolist()
@@ -43,10 +49,10 @@ def sankey_graph():
             link = dict(
             source = source_index,
             target = target_index,
-            value = value_1
+            value=value_1,
         ))])
-        fig.update_layout(title_text="", font_size=10)
-        #file_name = f.split("/")[-1].split(".")[0]
+        fig.update_layout(title_text=col.iloc[0][4], font_size=10)
+
         file_name = os.path.split(f.split(".")[0])
-        #fig.write_html(file_name+".html")
+
         fig.write_image(path + '/graphs/sankey/' + file_name[1] + ".png")
